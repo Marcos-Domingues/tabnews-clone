4d36e3c354ce5946a7d5cd45c3014b5c0263099d
Marcos Domingues <55745707+Marcos-Domingues@users.noreply.github.com>
2024-11-17 16:29:14 +0000

    makes `database.js` `client.connection()` mroe robust to errors

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,5 @@
 test("GET to /api/v1/status should return 200", async () => {
-   const response = await fetch("http://localhost:3002/api/v1/status")
+   const response = await fetch("http://localhost:3000/api/v1/status")
    expect(response.status).toBe(200);
 
    const responseBody = await response.json();
@@ -10,5 +10,5 @@ test("GET to /api/v1/status should return 200", async () => {
       
       expect(responseBody.dependencies.database.version).toEqual("16.0")
       expect(responseBody.dependencies.database.max_connections).toEqual(100)
-      expect(responseBody.dependencies.database.opened_connections).toEqual(37)
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1)
 });
